be9f2e9c9988f84547748593bfe7b2a5a0fc4fb0
Cjr37 <carlosclase37@gmail.com>
2024-01-09 14:39:30 +0100

    cambios service

--- src/app/services/tutorial.service.ts
+++ src/app/services/tutorial.service.ts
@@ -3,29 +3,31 @@ import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {Tarea} from "../models/tarea";
 
-const baseUrl ="http://localhost:5000/tareas"
 @Injectable({
   providedIn: 'root'
 })
 export class TutorialService {
 
+  private baseUrl = "http://localhost:5000/tareas"
+
   constructor(private httpClient: HttpClient) { }
 
   getTareas():Observable<Tarea[]>{
-    return this.httpClient.get<Tarea[]>(baseUrl)
+    return this.httpClient.get<Tarea[]>(this.baseUrl)
   }
 
   setTarea(tarea:Tarea){
-    this.httpClient.put(baseUrl,tarea)
+    this.httpClient.put(this.baseUrl, tarea)
   }
 
   deleteTarea(id:number){
-    let url = `${baseUrl}/${id}`
-    this.httpClient.delete(url)
+    /*let url = `${this.baseUrl}/${id}`
+    this.httpClient.delete(url)*/
+    return this.httpClient.delete(this.baseUrl + "/" + id)
   }
 
   modifyTarea(id: number, tarea: Tarea) {
-    let url = `${baseUrl}/${id}`
+    let url = `${this.baseUrl}/${id}`
     this.httpClient.put(url, tarea)
   }
 }
